d64e5136a73aa0ad96bdc932b91b1f0b95d7f144
Kymesa <Kymesa@unicesar.edu.co>
2023-09-29 15:52:01 -0500

    .

--- app/index.jsx
+++ app/index.jsx
@@ -25,7 +25,7 @@ export default function Feed() {
       descripcion:
         "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis alias maxime suscipit architecto ea? Asperiores, vero nemo?",
       urlPerfil:
-        "https://scontent.feoh2-1.fna.fbcdn.net/v/t39.30808-6/348585370_736933431767249_7487258338953250947_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=a2f6c7&_nc_eui2=AeETjGAToVaEk1ZOuD-PIA92x6o8e0DguV7Hqjx7QOC5XuFjOGje9nFwL3ljPYMFG1twjIo4YYYG4ADc3JJfbuET&_nc_ohc=evb3dC2fmlgAX8mmI4J&_nc_ht=scontent.feoh2-1.fna&oh=00_AfDmrWYwUEl0ySsImLEvAjc-3aubXXnfYIlX-wXQXVITHg&oe=65102822",
+        "https://images.unsplash.com/photo-1549692520-acc6669e2f0c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTR8fHByb2dyYW1tZXJ8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60",
       urlPost:
         "https://images.unsplash.com/photo-1512941937669-90a1b58e7e9c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8ZGV2ZWxvbWVwJTIwbW92aWx8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60",
     },
@@ -36,7 +36,7 @@ export default function Feed() {
       descripcion:
         "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis alias maxime suscipit architecto ea? Asperiores, vero nemo?",
       urlPerfil:
-        "https://scontent.feoh2-1.fna.fbcdn.net/v/t39.30808-6/348585370_736933431767249_7487258338953250947_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=a2f6c7&_nc_eui2=AeETjGAToVaEk1ZOuD-PIA92x6o8e0DguV7Hqjx7QOC5XuFjOGje9nFwL3ljPYMFG1twjIo4YYYG4ADc3JJfbuET&_nc_ohc=evb3dC2fmlgAX8mmI4J&_nc_ht=scontent.feoh2-1.fna&oh=00_AfDmrWYwUEl0ySsImLEvAjc-3aubXXnfYIlX-wXQXVITHg&oe=65102822",
+        "https://images.unsplash.com/photo-1549692520-acc6669e2f0c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTR8fHByb2dyYW1tZXJ8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60",
       urlPost:
         "https://cdn.pixabay.com/photo/2022/10/04/21/25/vr-7499157_640.jpg",
     },
