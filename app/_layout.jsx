bf13834107d0556b4ed46574b7557e4b32ad3711
KeinerMeza04 <Pacoabv2@gmail.com>
2023-10-02 19:27:24 -0500

    MODIFIQUE EL NOMBRE AL APP A V10

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -45,7 +45,7 @@ export default function Layout() {
             />
           ),
           headerTintColor: "rgba(215,121,255,1)",
-          title: "Friendy",
+          title: "Friendy V10",
           tabBarLabel: "Feed",
           tabBarIcon: () => (
             <Icon
