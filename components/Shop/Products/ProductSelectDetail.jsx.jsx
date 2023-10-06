2224189b4419ae86d742dee44aabef536c9fae7c
Kymesa <Kymesa@unicesar.edu.co>
2023-10-06 11:46:05 -0500

    ordenando los componentes 1.0

--- components/Shop/Products/ProductSelectDetail.jsx.jsx
+++ components/Shop/Products/ProductSelectDetail.jsx.jsx
@@ -1,11 +1,165 @@
-import { View, Text } from "react-native";
+import { View, Text, TouchableOpacity } from "react-native";
 import React from "react";
-
-const ProductModalDetails = () => {
+import Modal from "react-native-modal";
+import { Button, Icon, Image } from "react-native-magnus";
+import { AlertNotificationRoot } from "react-native-alert-notification";
+import {
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp,
+} from "react-native-responsive-screen";
+const ProductModalDetails = ({
+  productSelectModal,
+  isModalVisible,
+  setModalVisible,
+  handleCartAddProduct,
+}) => {
   return (
-    <View>
-      <Text>ProductModalDetails</Text>
-    </View>
+    <>
+      {productSelectModal && (
+        <View style={{ flex: 1 }}>
+          <Modal
+            // statusBarTranslucent={true}
+
+            animationIn={"fadeInUp"}
+            isVisible={isModalVisible}
+            style={{
+              backgroundColor: "white",
+              borderTopLeftRadius: 20,
+              borderTopRightRadius: 20,
+              margin: 0,
+              marginTop: 20,
+            }}
+          >
+            <View style={{ flex: 1 }}>
+              <View
+                style={{
+                  justifyContent: "center",
+                  alignItems: "center",
+                  flexDirection: "row",
+                }}
+              >
+                <TouchableOpacity
+                  style={{ position: "absolute", left: 10, top: 8 }}
+                  onPress={() => setModalVisible(!isModalVisible)}
+                >
+                  <Icon
+                    name="close"
+                    color="black"
+                    fontSize={20}
+                    fontFamily="AntDesign"
+                    bg="gray300"
+                    p={7}
+                    rounded={"circle"}
+                  />
+                </TouchableOpacity>
+
+                <Text
+                  style={{ marginTop: 12, fontWeight: "bold", fontSize: 19 }}
+                >
+                  {productSelectModal.name}
+                </Text>
+              </View>
+              <Image
+                mt={40}
+                alignSelf="center"
+                h={"20%"}
+                w={"60%"}
+                m={10}
+                rounded={20}
+                resizeMode="contain"
+                source={{
+                  uri: productSelectModal.imgUrl,
+                }}
+              />
+              <Text
+                style={{
+                  marginLeft: 15,
+                  fontWeight: "800",
+                  fontSize: 21,
+                  marginTop: 22,
+                }}
+              >
+                {productSelectModal.name}
+              </Text>
+              <View style={{ flexDirection: "row", alignItems: "center" }}>
+                <Text
+                  style={{
+                    marginLeft: 15,
+                    fontWeight: "bold",
+                    fontSize: 17,
+                    marginTop: 12,
+                  }}
+                >
+                  ${productSelectModal.price}
+                </Text>
+                <Text
+                  style={{
+                    marginLeft: 9,
+                    fontWeight: "400",
+                    fontSize: 14,
+                    marginTop: 12,
+                    textDecorationLine: "line-through",
+                  }}
+                >
+                  ${"10.000"}
+                </Text>
+              </View>
+              <Text
+                style={{
+                  textAlign: "center",
+                  fontSize: 17,
+                  fontWeight: "800",
+                  marginTop: 15,
+                }}
+              >
+                Descripcion del producto
+              </Text>
+
+              <Text
+                style={{
+                  marginHorizontal: wp(4),
+                  fontWeight: "600",
+                  fontSize: 15,
+                  lineHeight: 25,
+                }}
+              >
+                {"\n"} ✅ Lorem ipsum dolor sit amet consectetur adipisicing .
+                {"\n"} ✅ Illo molestiae, blanditiis ea quos laborum nemo beatae
+                .{"\n"} ✅ voluptatem asperiores ipsam saepe recusandae. {"\n"}{" "}
+                ✅ quas quia necessitatibus suscipit placeat.
+              </Text>
+              <View
+                style={{
+                  position: "absolute",
+                  bottom: hp(8),
+                  alignSelf: "center",
+                }}
+              >
+                <AlertNotificationRoot theme="light" dialogConfig={"autoClose"}>
+                  <Button
+                    onPress={() => handleCartAddProduct(productSelectModal)}
+                    bg="#28D885"
+                    color="white"
+                    fontWeight="bold"
+                    suffix={
+                      <Icon
+                        name="cart-plus"
+                        ml="md"
+                        color="white"
+                        fontFamily="FontAwesome"
+                        fontSize={20}
+                      />
+                    }
+                  >
+                    Add Cart
+                  </Button>
+                </AlertNotificationRoot>
+              </View>
+            </View>
+          </Modal>
+        </View>
+      )}
+    </>
   );
 };
 
--- components/Shop/Products/Products.jsx
+++ components/Shop/Products/Products.jsx
@@ -1,22 +1,16 @@
 import { View, Text, FlatList, TouchableOpacity } from "react-native";
 import GestureRecognizer from "react-native-swipe-gestures";
-import {
-  widthPercentageToDP as wp,
-  heightPercentageToDP as hp,
-} from "react-native-responsive-screen";
+import { widthPercentageToDP as wp } from "react-native-responsive-screen";
 import React, { useContext, useEffect, useState } from "react";
 import ProductsData from "./productsData";
 import ProductCard from "./ProductCard";
 import Modal from "react-native-modal";
-import { Button, Div, Icon, Image } from "react-native-magnus";
-import {
-  ALERT_TYPE,
-  Dialog,
-  AlertNotificationRoot,
-} from "react-native-alert-notification";
+import { Div, Icon, Image } from "react-native-magnus";
+import { ALERT_TYPE, Dialog } from "react-native-alert-notification";
 import { ModalCartContext } from "../contexts/ModalCartContext";
-import { Link, router } from "expo-router";
+import { Link } from "expo-router";
 import { ScrollView } from "react-native-virtualized-view";
+import ProductModalDetails from "./ProductSelectDetail.jsx";
 const categoriesList = [
   "Smartphones",
   "Laptops",
@@ -68,7 +62,7 @@ const Products = () => {
       setCardProducts([...cardProducts, productAddCard]);
       setCountCart(countCart + 1);
       setModalVisible(!isModalVisible);
-    }, 1000);
+    }, 900);
   };
   const handleClearCard = () => {
     setCardProducts([]);
@@ -122,153 +116,17 @@ const Products = () => {
         </View>
       )}
 
-      {productSelectModal && (
-        <View style={{ flex: 1 }}>
-          <Modal
-            // statusBarTranslucent={true}
+      {productSelectModal ? (
+        <ProductModalDetails
+          productSelectModal={productSelectModal}
+          isModalVisible={isModalVisible}
+          setModalVisible={setModalVisible}
+          handleCartAddProduct={handleCartAddProduct}
+        />
+      ) : null}
 
-            animationIn={"fadeInUp"}
-            isVisible={isModalVisible}
-            style={{
-              backgroundColor: "white",
-              borderTopLeftRadius: 20,
-              borderTopRightRadius: 20,
-              margin: 0,
-              marginTop: 20,
-            }}
-          >
-            <View style={{ flex: 1 }}>
-              <View
-                style={{
-                  justifyContent: "center",
-                  alignItems: "center",
-                  flexDirection: "row",
-                }}
-              >
-                <TouchableOpacity
-                  style={{ position: "absolute", left: 10, top: 8 }}
-                  onPress={() => setModalVisible(!isModalVisible)}
-                >
-                  <Icon
-                    name="close"
-                    color="black"
-                    fontSize={20}
-                    fontFamily="AntDesign"
-                    bg="gray300"
-                    p={7}
-                    rounded={"circle"}
-                  />
-                </TouchableOpacity>
-
-                <Text
-                  style={{ marginTop: 12, fontWeight: "bold", fontSize: 19 }}
-                >
-                  {productSelectModal.name}
-                </Text>
-              </View>
-              <Image
-                mt={40}
-                alignSelf="center"
-                h={"20%"}
-                w={"60%"}
-                m={10}
-                rounded={20}
-                resizeMode="contain"
-                source={{
-                  uri: productSelectModal.imgUrl,
-                }}
-              />
-              <Text
-                style={{
-                  marginLeft: 15,
-                  fontWeight: "800",
-                  fontSize: 21,
-                  marginTop: 22,
-                }}
-              >
-                {productSelectModal.name}
-              </Text>
-              <View style={{ flexDirection: "row", alignItems: "center" }}>
-                <Text
-                  style={{
-                    marginLeft: 15,
-                    fontWeight: "bold",
-                    fontSize: 17,
-                    marginTop: 12,
-                  }}
-                >
-                  ${productSelectModal.price}
-                </Text>
-                <Text
-                  style={{
-                    marginLeft: 9,
-                    fontWeight: "400",
-                    fontSize: 14,
-                    marginTop: 12,
-                    textDecorationLine: "line-through",
-                  }}
-                >
-                  ${"10.000"}
-                </Text>
-              </View>
-              <Text
-                style={{
-                  textAlign: "center",
-                  fontSize: 17,
-                  fontWeight: "800",
-                  marginTop: 15,
-                }}
-              >
-                Descripcion del producto
-              </Text>
-
-              <Text
-                style={{
-                  marginHorizontal: wp(4),
-                  fontWeight: "600",
-                  fontSize: 15,
-                  lineHeight: 25,
-                }}
-              >
-                {"\n"} ✅ Lorem ipsum dolor sit amet consectetur adipisicing .
-                {"\n"} ✅ Illo molestiae, blanditiis ea quos laborum nemo beatae
-                .{"\n"} ✅ voluptatem asperiores ipsam saepe recusandae. {"\n"}{" "}
-                ✅ quas quia necessitatibus suscipit placeat.
-              </Text>
-              <View
-                style={{
-                  position: "absolute",
-                  bottom: hp(8),
-                  alignSelf: "center",
-                }}
-              >
-                <AlertNotificationRoot theme="light" dialogConfig={"autoClose"}>
-                  <Button
-                    onPress={() => handleCartAddProduct(productSelectModal)}
-                    bg="#28D885"
-                    color="white"
-                    fontWeight="bold"
-                    suffix={
-                      <Icon
-                        name="cart-plus"
-                        ml="md"
-                        color="white"
-                        fontFamily="FontAwesome"
-                        fontSize={20}
-                      />
-                    }
-                  >
-                    Add Cart
-                  </Button>
-                </AlertNotificationRoot>
-              </View>
-            </View>
-          </Modal>
-        </View>
-      )}
       <GestureRecognizer
         style={{ flex: 1 }}
-        // onSwipeUp={() => setModalCart(true)}
         onSwipeDown={() => setModalCart(!modalCart)}
       >
         <View style={{ flex: 1 }}>
@@ -442,13 +300,6 @@ const Products = () => {
                   },
                 }}
               >
-                {/* <Pressable
-                style={{
-                  backgroundColor: "#28D885",
-                  padding: 15,
-                  borderRadius: 18,
-                }}
-              > */}
                 <Div bg="#28D885" p={15} rounded={18} row alignItems="center">
                   <Text
                     style={{ fontWeight: "bold", fontSize: 15 }}
