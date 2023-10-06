79e3285c0e765a9625dbc5570b07f31073d605a3
Kymesa <Kymesa@unicesar.edu.co>
2023-10-06 11:49:27 -0500

    .

---
+++ components/Shop/Products/ProductsCarts.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const ProductsCarts = () => {
+  return (
+    <View>
+      <Text>ProductsCarts</Text>
+    </View>
+  );
+};
+
+export default ProductsCarts;
