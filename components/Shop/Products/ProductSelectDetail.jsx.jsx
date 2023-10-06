1fd55080c5f992d2f928be9fd8eafbcfc35ed997
Kymesa <Kymesa@unicesar.edu.co>
2023-10-06 11:35:49 -0500

    .

--- components/Shop/Products/ProductSelectDetail.jsx.jsx
+++ components/Shop/Products/ProductSelectDetail.jsx.jsx
@@ -1,12 +1,12 @@
 import { View, Text } from "react-native";
 import React from "react";
 
-const ProductFIlter = () => {
+const ProductModalDetails = () => {
   return (
     <View>
-      <Text>ProductFIlter</Text>
+      <Text>ProductModalDetails</Text>
     </View>
   );
 };
 
-export default ProductFIlter;
+export default ProductModalDetails;
